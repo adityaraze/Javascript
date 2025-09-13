acd85bce9d70fa7501759523331522708b9a242a
Aditya Modanwal <135428914+adityaraze@users.noreply.github.com>
2025-09-13 09:40:27 +0000

    varibles file change

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+var accountName = "Aditya"
+let acountEmail = "aditya@gmail.com"
+accountCity = "jaipur"
+let accountStatus
+
+// accountId = 2;
+
+accountName = "Raj"
+acountEmail = "raj@gmail.com"
+accountCity = "Bengluru"
+
+/*
+Prefer not to use var because of scope problem
+*/
+
+console.table([accountId,accountName,acountEmail,accountCity,accountStatus])
