eaa3f2c95bdd68b3c6a4d962c0362a394ebb54b8
Aditya Modanwal <135428914+adityaraze@users.noreply.github.com>
2025-09-13 10:05:40 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict" // treate all js code as newer version
+
+// alert(3+3)  // we are using node js not browser
+
+let name = "aditya"
+let age = 18
+let isLoggedIn = false
+let state
+
+// numbers => 2^53
+// bigInt
+// string ""
+// boolean
+// null -> standalone value
+// undefined
+// symbol
+
+// object
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+
