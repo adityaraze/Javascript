0ce14502b1951f172176915c9034d429c0cc3b46
Aditya Modanwal <135428914+adityaraze@users.noreply.github.com>
2025-09-13 11:39:56 +0000

    static memory and heap memory

--- 01_basics/dataTypesSummary.js
+++ 01_basics/dataTypesSummary.js
@@ -12,7 +12,7 @@ const outSideTemp = null
 const id = Symbol("123")
 const anotherId = Symbol("123")
 
-console.log(id == anotherId) // false
+// console.log(id == anotherId) // false
 
 // Reference
 
@@ -25,10 +25,34 @@ let myObj = {
 }
 
 const myFunc = function (){
-    console.log("Hello World")
+    // console.log("Hello World")
 }
 
-console.log(typeof anotherId)
+// console.log(typeof anotherId)
 
-console.log("typeof null = ",typeof null)
-console.log("typeof function = ",typeof myFunc) // function [object function]
+// console.log("typeof null = ",typeof null)
+// console.log("typeof function = ",typeof myFunc) // function [object function]
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Static [Premitive] Heap[Non-Premitive] 
+
+let myname = "aditya"
+
+let anotherName = myname
+anotherName = "raj"
+console.log(myname)
+console.log(anotherName)
+
+let userOne = {
+    email:"adityamodanwal@gmail.com",
+    upi:"7379742138@naviaxis"
+}
+
+let userTwo = userOne // userTwo is pointing to memory location of userOne
+
+userTwo.email = "raj@google.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
