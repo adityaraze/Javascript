76d7e8577e655de9104de3be8ef890820676deb8
Aditya Modanwal <135428914+adityaraze@users.noreply.github.com>
2025-09-13 11:08:03 +0000

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 == 1)
+// console.log(2 != 1)
+// console.log(2 >= 1)
+// console.log(2 <= 1)
+// console.log(2 > 1)
+// console.log(2 < 1)
+
+// console.log("2" > 1)
+// console.log("2" < 1)
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// console.log(undefined > 0)
+// console.log(undefined == 0)
+// console.log(undefined >= 0) // all false
+
+// strict check
+
+console.log("2" == 2) // true
+console.log("2" === 2) // check the value as well as datatype
